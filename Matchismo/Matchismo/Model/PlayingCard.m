db89789bace1efe3d23e087c629efd3704175c14
Patrick Quirk <quirker@gmail.com>
2014-01-11 17:56:26 -0500

    Fixed content and suit setter to be more correct

--- Matchismo/Matchismo/Model/PlayingCard.m
+++ Matchismo/Matchismo/Model/PlayingCard.m
@@ -12,7 +12,22 @@
 
 - (NSString*)contents
 {
-    return [NSString stringWithFormat:@"%d%@", self.rank, self.suit];
+    NSArray* rankStrings = @[@"?",@"A",@"2",@"3",@"4",@"5",@"6",@"7",@"8",@"9",@"10",@"J",@"Q",@"K"];
+    return [rankStrings[self.rank] stringByAppendingString:self.suit];
+}
+
+@synthesize suit = _suit;
+
+- (void)setSuit:(NSString *)suit
+{
+    NSArray* validSuits = @[@"♣︎",@"♠︎",@"♥︎",@"♦︎"];
+    if ([validSuits containsObject:suit])
+        _suit = suit;
+}
+
+- (NSString*)suit
+{
+    return _suit ? _suit : @"?";
 }
 
 @end
