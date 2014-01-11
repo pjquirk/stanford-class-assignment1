6283dc63e88de6ae4328174aa9506ecfad389843
Patrick Quirk <quirker@gmail.com>
2014-01-11 18:07:47 -0500

    Added Deck and PlayingCardDeck

---
+++ Matchismo/Matchismo/Model/Deck.m
@@ -0,0 +1,13 @@
+//
+//  Deck.m
+//  Matchismo
+//
+//  Created by Patrick Quirk on 1/11/14.
+//  Copyright (c) 2014 Patrick Quirk. All rights reserved.
+//
+
+#import "Deck.h"
+
+@implementation Deck
+
+@end
---
+++ Matchismo/Matchismo/Model/PlayingCardDeck.m
@@ -0,0 +1,13 @@
+//
+//  PlayingCardDeck.m
+//  Matchismo
+//
+//  Created by Patrick Quirk on 1/11/14.
+//  Copyright (c) 2014 Patrick Quirk. All rights reserved.
+//
+
+#import "PlayingCardDeck.h"
+
+@implementation PlayingCardDeck
+
+@end
